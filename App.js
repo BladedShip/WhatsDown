90ee03b6249c189f70c0000c32fdbfd803b8616b
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-02-06 15:41:55 +0530

    User management

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './src/navigation';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import awsconfig from './src/aws-exports';
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
 
-Amplify.configure({...awsconfig, Analytics: { disabled: true } });
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 function App() {
+
+  useEffect(() => {
+    const syncUser = async () => {
+
+      //get Auth user
+      const authUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+      //query db using ID
+      const userData = await API.graphql(graphqlOperation(getUser, { id: authUser.attributes.sub }));
+
+      //if user exists, do nothing:else create user in db
+      if (userData.data.getUser) {
+        console.log("User already exists in DB");
+        return;
+      }
+
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+        status: "WhatsDown, homies?",
+      };
+
+      const newUserResponse = await API.graphql(graphqlOperation(
+        createUser, { input: newUser }
+      ));
+
+    }
+    syncUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
