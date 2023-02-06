fa4ade38d2df9905a7e21763b5eeb6f49b9e2b32
BladedShip <67158370+BladedShip@users.noreply.github.com>
2023-02-06 22:15:50 +0530

    idk

--- src/screens/Chats/queries.js
+++ src/screens/Chats/queries.js
@@ -1,6 +1,6 @@
 export const listChatRooms = `
-    query MyQuery{
-        getUser(id:"ff17fac6-70b3-4b63-b2a0-8feaa39ef509"){
+    query GetUser($id: ID!) {
+        getUser(id: $id) {
             id
             ChatRooms{
                 items{
